f65123bcd548a7b168168e4be2e1457bb4cc50c4
Djonathan Barros <djonathan.barros@gmail.com>
2018-07-24 14:14:31 -0300

    added mockito injection initialization to RestDataTest

--- src/test/java/com/springRest/demo/springRest/ControllerTest/RestDataTest.java
+++ src/test/java/com/springRest/demo/springRest/ControllerTest/RestDataTest.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -26,7 +27,7 @@ import com.springRest.demo.SpringRest.service.RestServiceEnd;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class RestDataTest {
 
-	
+
 	private MockMvc mvc;
 
 	@MockBean
@@ -37,19 +38,20 @@ public class RestDataTest {
 
 	@Before
 	public void steUP() throws Exception {
+		MockitoAnnotations.initMocks(this);
 		mvc = MockMvcBuilders.standaloneSetup(restdata).build();
 	}
 
 	@Test
 	public void testGetNthFibonacci() throws Exception {
 
-//		//when(restServiceEnd.getNthFebbonic(10L)).thenReturn(55);
-//
-//		mvc.perform(get("/api/Fibonacci?n=10")).andExpect(status().isOk())
-//				.andExpect(content().contentType("application/json;charset=UTF-8"))
-//				.andExpect(header().string("pragma", "no-cashe"))
-//				.andExpect(content().json(AbstractRestControllerTest.asJsonString(55)));
-		fail("Not yet implemented");
+		when(restServiceEnd.getNthFebbonic(10L)).thenReturn(55);
+
+		mvc.perform(get("/api/Fibonacci?n=10")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(header().string("pragma", "no-cache"))
+				.andExpect(content().json(AbstractRestControllerTest.asJsonString(55)));
+//		fail("Not yet implemented");
 
 	}
 
